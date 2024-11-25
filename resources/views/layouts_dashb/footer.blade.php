b19b9607f2965c48a2ab9a4f6d7f8ccd84b11146
chedy <chedy.khachlouf@example.com>
2024-11-25 18:35:45 +0100

    na7it ili zeyed fel dashb

--- resources/views/layouts_dashb/footer.blade.php
+++ resources/views/layouts_dashb/footer.blade.php
@@ -3,25 +3,12 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>copyright &copy;
-                <script>
-                    document.write(new Date().getFullYear());
-                </script> - developed by
-                <b><a href="https://indrijunanda.gitlab.io/" target="_blank">indrijunanda</a></b>
+            <span>developed by
+                <b><a href="https://www.linkedin.com/in/chedy-khachlouf-429399204/" target="_blank">KHACHLOUF Chedy</a></b>
             </span>
         </div>
     </div>
 
-    <div class="container my-auto py-2">
-        <div class="copyright text-center my-auto">
-            <span>copyright &copy;
-                <script>
-                    document.write(new Date().getFullYear());
-                </script> - distributed by
-                <b><a href="https://themewagon.com/" target="_blank">themewagon</a></b>
-            </span>
-        </div>
-    </div>
 </footer>
 <!-- Footer -->
 </div>
